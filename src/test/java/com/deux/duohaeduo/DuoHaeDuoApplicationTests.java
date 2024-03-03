c05401050e7c033a4dabf6df51f085ee69261209
Woogie95 <choisw950928@gmail.com>
2024-03-03 20:42:23 +0900

    Initial Commit

---
+++ src/test/java/com/deux/duohaeduo/DuoHaeDuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deux.duohaeduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuoHaeDuoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
