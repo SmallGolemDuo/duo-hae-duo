cc7adbc62430ffa0d4705f6e93202be4edb9e9ad
Woogie95 <choisw950928@gmail.com>
2024-03-24 18:48:59 +0900

    chore: ChampionRequest 변수 수정

--- src/main/java/com/deux/duohaeduo/dto/request/ChampionRequest.java
+++ src/main/java/com/deux/duohaeduo/dto/request/ChampionRequest.java
@@ -4,21 +4,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @Builder
 @RequiredArgsConstructor
 public class ChampionRequest {
 
-    private final String difficulty;
-
-    private final String damageType;
-
-    private final String attackType;
-
-    private final String championType;
-
-    private final String ultimateType;
-
-    private final String skillDetails;
+    private final List<String> recommendItems;
 
 }
