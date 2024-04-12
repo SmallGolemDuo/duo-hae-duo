15ebcf7e594e713a43adcc4b27c7010841830715
Woogie95 <choisw950928@gmail.com>
2024-04-12 21:41:05 +0900

    chore: 빌드 테스트를 위한 테스트 코드 주석

--- src/test/java/com/deux/duohaeduo/DuoHaeDuoApplicationTests.java
+++ src/test/java/com/deux/duohaeduo/DuoHaeDuoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.deux.duohaeduo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DuoHaeDuoApplicationTests {
-
-  @Test
-  void contextLoads() {
-  }
-
-}
+//package com.deux.duohaeduo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DuoHaeDuoApplicationTests {
+//
+//  @Test
+//  void contextLoads() {
+//  }
+//
+//}
