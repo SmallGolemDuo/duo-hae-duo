4bde3c10d07a47679c48acc391b39c2b9279d662
Meringue-KIm <dd8873@naver.com>
2024-04-14 16:09:20 +0900

    chore: NotNull 어노테이션 수정

--- src/main/java/com/deux/duohaeduo/dto/request/FindGameUserInfoRequest.java
+++ src/main/java/com/deux/duohaeduo/dto/request/FindGameUserInfoRequest.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.antlr.v4.runtime.misc.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @Builder
