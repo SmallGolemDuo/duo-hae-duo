2804f3b47a36b5bac04043b709fd9f9b116e018a
Woogie95 <choisw950928@gmail.com>
2024-04-24 10:50:06 +0900

    chore: 라인 정리

--- src/main/java/com/deux/duohaeduo/service/SingleService.java
+++ src/main/java/com/deux/duohaeduo/service/SingleService.java
@@ -26,15 +26,12 @@ public class SingleService {
     public FindGameMemberInfoResponse findGameUserInfo(FindGameUserInfoRequest findGameUserInfoRequest) throws IOException, JSONException {
         long matchCount = 20L;
         List<String> nicknameAndTag = List.of(findGameUserInfoRequest.getGameNickname().split("#"));
-        System.out.println(nicknameAndTag.get(0));
-
         String tag;
         if (nicknameAndTag.size() != 1) {
             tag = nicknameAndTag.get(1);
         } else {
             tag = "KR1";
         }
-
         FindGameMemberInfo findGameMemberInfo = riotService.findRiotGameMemberInfo(nicknameAndTag.get(0), tag, 1L, matchCount);
         return new FindGameMemberInfoResponse(groupService.convertRiotSummonerInfo(findGameMemberInfo, nicknameAndTag.get(0), tag));
     }
