267d63912235163d5cb56fc1e10ae42f34d5b1c9
Woogie95 <choisw950928@gmail.com>
2024-04-30 19:54:17 +0900

    feat: 댓글 생성 API 추가

---
+++ src/main/java/com/deux/duohaeduo/entity/Comment.java
@@ -0,0 +1,40 @@
+package com.deux.duohaeduo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+
+    private String password;
+
+    private String content;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    public void addPost(Post post) {
+        this.post = post;
+    }
+
+}
